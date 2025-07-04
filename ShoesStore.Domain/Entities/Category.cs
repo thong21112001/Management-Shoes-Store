1c86ab46103fa5526878d336a7bf58e6042f6f45
PanPan <quangthong211101@gmail.com>
2025-07-04 10:25:44 +0700

    Điều chỉnh hiển thị chi tiết và xóa bằng modal dùng partialview, add Toastr hiển thị thông báo

--- ShoesStore.Domain/Entities/Category.cs
+++ ShoesStore.Domain/Entities/Category.cs
@@ -23,10 +23,10 @@ public partial class Category
             : "https://placehold.co/650x350/E8F5E8/7cb342?text=Featured+Article";
 
 
-    [Display(Name = "Ngày tạo")]
+    [Display(Name = "Ngày tạo"), DataType(DataType.Date)]
     public DateTime? CreatedAt { get; set; }
 
-    [Display(Name = "Ngày cập nhập")]
+    [Display(Name = "Ngày cập nhập"), DataType(DataType.Date)]
     public DateTime? UpdatedAt { get; set; }
 
     public virtual ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
--- ShoesStore.Web/Pages/Categories/Index.cshtml.cs
+++ ShoesStore.Web/Pages/Categories/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using ShoesStore.Application.Common.Interfaces;
 using ShoesStore.Application.DTOs;
+using ShoesStore.Domain.Entities.Data;
 
 namespace ShoesStore.Web.Pages.Categories
 {
@@ -19,12 +21,69 @@ namespace ShoesStore.Web.Pages.Categories
         {
             if (_categoryRepository != null)
             {
-                Categories = await _categoryRepository.GetAllAsync();
+                Categories = await GetDataAsync();
             }
             else
             {
                 Categories = new List<CategoriesDto>();
             }
         }
+
+
+
+        #region Phần Chi tiết và Xóa
+        //Lấy thông tin đẩy lên partial: _DetailsPartial
+        public async Task<IActionResult> OnGetDetailsPartial(int id)
+        {
+            var cate = await GetCategoryById(id);
+
+            if (cate == null)
+                return NotFound();
+
+            return Partial("_DetailsPartial", cate);
+        }
+
+
+        //Lấy thông tin đẩy lên partial: _DeletePartial
+        public async Task<IActionResult> OnGetDeletePartial(int id)
+        {
+            var cate = await GetCategoryById(id);
+
+            if (cate == null)
+                return NotFound();
+
+            return Partial("_DeletePartial", cate);
+        }
+
+
+        //Tiến hành kiểm tra và xóa
+        public async Task<IActionResult> OnPostDeleteCategoryAsync(int id)
+        {
+            var cate = await GetCategoryById(id);
+
+            if (cate == null)
+            {
+                return new JsonResult(new { success = false, message = "Không tìm thấy dữ liệu cần xóa!" });
+            }
+
+            await _categoryRepository.DeleteAsync(id);
+
+            return new JsonResult(new { success = true });
+        }
+        #endregion
+
+
+
+        #region Viết phương thức xử lý
+        private async Task<Category?> GetCategoryById(int id)
+        {
+            return await _categoryRepository.GetByIdAsync(id);
+        }
+
+        private async Task<IList<CategoriesDto>> GetDataAsync()
+        {
+            return await _categoryRepository.GetAllAsync();
+        }
+        #endregion
     }
 }
